71e897056de946f4aaa832b9c467c8a89eca1fb0
Nathan Brachotte <nathan@colorfy.me>
2019-03-05 18:12:19 +0100

    nfo

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -28,12 +28,12 @@ module.exports = {
       })
       .catch(err => { throw err })
   },
-  login: async ({ enmail, password }) => {
-    const user = User.findOne({ email })
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email })
     if (!user) throw new Error('User does not exist');
     const isEqual = await bcrypt.compare(password, user.password)
     if (!isEqual) throw new Error('Password is incorrect');
-    jwt.sign({ userId: user.id, email: user.email }, 'privateKey', { expiresIn: '12h' });
-
+    const token = jwt.sign({ userId: user.id, email: user.email }, 'privateKey', { expiresIn: '12h' });
+    return { userId: user.id, token, tokenExpiration: 12 }
   }
 }
